6b8ee1beb32e2de7921386ab20a91d4c09ad1e74
Otuekong Idongesit <fizy500@gmail.com>
2023-09-08 16:03:15 +0100

    Added  javascript

--- index.js
+++ index.js
@@ -15,8 +15,29 @@ let days = [
 ];
 
 days.forEach((day, index) => {
-  // loop body
   if (index == new Date().getDay()) {
     dayOfTheWeek.innerHTML = day;
   }
 });
+
+// const now = new Date();
+
+// const year = now.getUTCFullYear();
+// const month = now.getUTCMonth() + 1;
+// const day = now.getUTCDate();
+// const hours = now.getUTCHours();
+// const minutes = now.getUTCMinutes();
+// const seconds = now.getUTCSeconds();
+// const milliseconds = now.getUTCMilliseconds();
+
+// const finalTime = `${year}-${month}-${day}
+//             ${hours}:${minutes}:${seconds}`;
+// currentUTCTime.innerHTML = finalTime;
+
+const updateTime = () => {
+  const currentUTCTimeInMilliseconds = new Date().getTime();
+  console.log(currentUTCTimeInMilliseconds);
+  currentUTCTime.textContent = currentUTCTimeInMilliseconds;
+};
+
+setInterval(updateTime, 1);
